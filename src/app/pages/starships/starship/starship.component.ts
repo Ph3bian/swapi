be7a83357da123f0a202fb7b0da96e11a1bc8d1d
Phebian Chukwurah <phebianchukwurah@gmail.com>
2019-04-16 02:45:50 +0100

    completed single planet page, added image files, created planet and starship components, updated routes to accomodate components

---
+++ src/app/pages/starships/starship/starship.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-starship',
+  templateUrl: './starship.component.html',
+  styleUrls: ['./starship.component.scss']
+})
+export class StarshipComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
